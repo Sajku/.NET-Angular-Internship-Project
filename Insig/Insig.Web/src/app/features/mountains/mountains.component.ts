1b2aeb044ff58df1800395a06c298461c80882ff
Sajku <kielek0412@gmail.com>
2022-07-13 22:16:13 +0200

    Add feature: it is possile to show and hide form

--- Insig/Insig.Web/src/app/features/mountains/mountains.component.ts
+++ Insig/Insig.Web/src/app/features/mountains/mountains.component.ts
@@ -7,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MountainsComponent implements OnInit {
     title = "Moutains Collection";
+    showAddForm: boolean = false
+    btnText: string = "Add a mountain";
+    btnIcon: string = "add";
 
     constructor() { }
 
     ngOnInit(): void { }
+
+    toggleAddFormVisibility(): void {
+        this.showAddForm = !this.showAddForm;
+        if (this.showAddForm) {
+            this.btnText = "Cancel form"
+            this.btnIcon = "close"
+        } else {
+            this.btnText = "Add a mountain"
+            this.btnIcon = "add"
+        }
+
+    }
 }
